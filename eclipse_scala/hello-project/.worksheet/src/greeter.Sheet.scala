1216b056aa8ae8df01fdecb64c89e5e471b1fe14
Sreejith Sreekantan03 <krssreejith@gmail.com>
2014-11-27 11:03:00 +0530

    Hello world for eclipse scala Reference: https://class.coursera.org/reactive-001/wiki/ToolsSetup

---
+++ eclipse_scala/hello-project/.worksheet/src/greeter.Sheet.scala
@@ -0,0 +1,8 @@
+package greeter
+
+object Sheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
+  println("Welcome to the Scala worksheet");$skip(11); 
+	val x = 5;System.out.println("""x  : Int = """ + $show(x ));$skip(30); 
+	def increase(i: Int) = i + 1;System.out.println("""increase: (i: Int)Int""");$skip(13); val res$0 = 
+	increase(x);System.out.println("""res0: Int = """ + $show(res$0))}
+}
